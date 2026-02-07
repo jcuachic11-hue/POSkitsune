a4c0fd44d06a0796477e3927875acd7da8da57d9
jean <j.cuachic11@gmail.com>
2026-02-07 00:44:41 -0600

    cambios en mysql.js2

--- scr/index.js
+++ scr/index.js
@@ -1,10 +1,16 @@
 // Cargar variables de entorno desde .env
 //require('dotenv').config();
+console.log("Cargando index.js...");
+
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 require('./bd/mysql');
+console.log("Cargando MySQL...");
+require('./bd/mysql');
+console.log("MySQL cargado");
+
 
 
 
@@ -15,10 +21,19 @@ const app = express();
 app.use(express.json());
 app.use(express.static('scr/publico'));
 
+console.log("Cargando rutas de auth...");
 const auth = require('./modulos/auth/rutas');
+
+console.log("Cargando rutas de productos...");
 const productos = require('./modulos/productos/rutas');
+
+console.log("Cargando rutas de usuarios...");
 const usuarios = require('./modulos/usuarios/rutas');
+
+console.log("Cargando rutas de ventas...");
 const ventas = require('./modulos/ventas/rutas');
+
+
 //const rutas = require('./rutas');
 
 
