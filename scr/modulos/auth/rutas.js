b0294ce6d2a47ce19804ee133d15f7e88dd07486
jean <j.cuachic11@gmail.com>
2026-02-06 21:52:28 -0600

    Agregada ruta GET / para mostrar login.html

--- scr/modulos/auth/rutas.js
+++ scr/modulos/auth/rutas.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
-const controlador = require('./controlador.js'); 
+const controlador = require('./controlador.js');
 
-// Ruta de login
+// Mostrar login.html en la raíz
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../publico/login.html'));
+});
+
+// Procesar login
 router.post('/login', controlador.login);
 
 module.exports = router;
+
