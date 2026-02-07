250b57dcec6a50beb2624b88513d1ca1507c0466
jean <j.cuachic11@gmail.com>
2026-02-06 23:41:43 -0600

    cambio nuevo

--- scr/modulos/auth/rutas.js
+++ scr/modulos/auth/rutas.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const controlador = require('./controlador.js');
 
-// Mostrar login.html en la raíz
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../publico/login.html'));
-});
+const controlador = require('./controlador');  // <--- SIN (db)
+
+// Mostrar login.html
+//router.get('/', (req, res) => {
+ // res.sendFile(path.join(__dirname, '../publico/login.html'));
+//});
 
 // Procesar login
 router.post('/login', controlador.login);
 
 module.exports = router;
-
