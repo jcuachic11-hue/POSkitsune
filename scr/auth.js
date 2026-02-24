44573e194e0f6fbb34c1a7e844471487008c3549
jean <j.cuachic11@gmail.com>
2026-02-23 23:21:45 -0600

    retorno 2

--- scr/auth.js
+++ scr/auth.js
@@ -1,4 +1,4 @@
-/*const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const claveSecreta = 'tu_clave_secreta';
 
 function verificarToken(req, res, next) {
@@ -14,29 +14,7 @@ function verificarToken(req, res, next) {
 }
 
 module.exports = { verificarToken, claveSecreta };
-*/
 
-// Cargar variables de entorno desde .env
-require('dotenv').config();
 
-const express = require('express');
-const app = express();
 
-app.use(express.json());
-app.use(express.static('scr/publico'));
-
-const auth = require('./modulos/auth/rutas');
-const productos = require('./modulos/productos/rutas');
-const usuarios = require('./modulos/usuarios/rutas');
-const ventas = require('./modulos/ventas/rutas');
-
-app.use('/', auth);
-app.use('/productos', productos);
-app.use('/usuarios', usuarios);
-app.use('/ventas', ventas);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
 
