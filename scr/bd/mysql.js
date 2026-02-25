10d0b6e37d10733e5e58e67591c87ca1868c385b
jean <j.cuachic11@gmail.com>
2026-02-24 22:50:08 -0600

    otro de mysql2

--- scr/bd/mysql.js
+++ scr/bd/mysql.js
@@ -1,11 +1,19 @@
-const mysql = require('mysql2/promise'); // <--- DEBE tener el /promise
+// bd/mysql.js
+const mysql = require('mysql2/promise'); // <--- REVISA ESTO: Debe tener '/promise'
 
-const pool = mysql.createPool({
+const dbConfig = {
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT || 3306,
-});
+    // Esto ayuda a que Railway no corte la conexión por inactividad
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
+
+// Crear el pool de conexiones
+const pool = mysql.createPool(dbConfig);
 
 module.exports = pool;
